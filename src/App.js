e8e1398302136e56d6e5d63fc787fa5185c148fb
Borguuh <abdrahmanadewale@gmail.com>
2020-06-03 23:32:23 +0530

    First

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Grid} from '@material-ui/core';
+import youtube from './api/youtube';
+ import SearchBar from './components/SearchBar' 
+ import VideoList from './components/VideoList'
+ import VideoDetail from './components/VideoDetail' 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+class App extends React.Component {
+
+    handleSubmit = async (searchTerm) =>{
+        const response = await youtube.get("search" , {
+            params: {
+                part : 'snippet',
+                maxResult : 5,
+                key: 'AIzaSyAo-xk9f-V9QDwX3rLA7xDHgdKsd_xX4R4',
+                q: searchTerm,
+        }});
+
+    }
+
+
+    render() {
+        return (
+            <div>
+                <Grid justify="center" container spacing={10}> 
+
+                <Grid item xs={12}>
+
+                    <Grid container spacig={16}>
+
+                        <Grid item xs={12}>
+                        <SearchBar onFormSubmit={this.handleSubmit}/>
+
+                        </Grid>
+                        
+                        <Grid item xs={8}>
+                         <VideoDetail />
+
+                        </Grid>
+                        <Grid item xs={4}>
+                               {/* Video List       */}
+                        </Grid>
+
+                    </Grid>
+
+                </Grid>
+                </Grid>
+            </div>
+        )
+    }
 }
 
 export default App;
---
+++ src/api/youtube.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+export default axios.create({
+    baseURL:'https://www.googleapis.com/youtube/v3',
+    params:{
+        part : 'snippet',
+        maxResult : 5,
+        key: 'AIzaSyAo-xk9f-V9QDwX3rLA7xDHgdKsd_xX4R4'
+    }
+});
